39e0bd65d1c6afb254013f2ad0effe7c6ebc150d
Snehendu Roy <roysnehendupersonal@gmail.com>
2025-10-14 21:39:43 +0530

    init rn with tailwind

---
+++ app/(tabs)/explore.tsx
@@ -0,0 +1,32 @@
+import { Image } from "expo-image";
+import { Platform, StyleSheet, View } from "react-native";
+
+import { Collapsible } from "@/components/ui/collapsible";
+import { ExternalLink } from "@/components/external-link";
+import ParallaxScrollView from "@/components/parallax-scroll-view";
+import { ThemedText } from "@/components/themed-text";
+import { ThemedView } from "@/components/themed-view";
+import { IconSymbol } from "@/components/ui/icon-symbol";
+import { Fonts } from "@/constants/theme";
+import { Text } from "react-native";
+
+export default function TabTwoScreen() {
+  return (
+    <View className="flex-1">
+      <Text className="text-2xl text-white">Hello World</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerImage: {
+    color: "#808080",
+    bottom: -90,
+    left: -35,
+    position: "absolute",
+  },
+  titleContainer: {
+    flexDirection: "row",
+    gap: 8,
+  },
+});
